cf7b6645cc31a736b09630582e3269b987c8cf50
Daniel Čech <daniel.cech@gmail.com>
2019-04-20 14:34:40 +0200

    [feat]: fixed code generators

--- Arm/Programs/_Resources/robot_block_generators.js
+++ Arm/Programs/_Resources/robot_block_generators.js
@@ -1,8 +1,20 @@
 'use strict';
 
+Blockly.JavaScript['enable_pump_inline'] = function(block) {
+    var value_enable = (block.getFieldValue('enable') == "TRUE")
+    var code = 'RobotCommands.enablePump(' + value_enable + ');\n';
+    return code;
+};
+
 Blockly.JavaScript['enable_pump'] = function(block) {
     var value_enable = Blockly.JavaScript.valueToCode(block, 'enable', Blockly.JavaScript.ORDER_ATOMIC);
     var code = 'RobotCommands.enablePump(' + value_enable + ');\n';
+    return code;
+};
+
+Blockly.JavaScript['pause_inline'] = function(block) {
+    var value_seconds = block.getFieldValue('seconds');
+    var code = 'RobotCommands.pause(' + value_seconds + ');\n';
     return code;
 };
 
@@ -12,36 +24,29 @@ Blockly.JavaScript['pause'] = function(block) {
     return code;
 };
 
+Blockly.JavaScript['move_inline'] = function(block) {
+    var value_x = block.getFieldValue('x');
+    var value_y = block.getFieldValue('y');
+    var value_z = block.getFieldValue('z');
+    var value_t = block.getFieldValue('t')
+    var code = 'RobotCommands.move(' + value_x + ', ' + value_y + ', ' + value_z + ', ' + value_t + ');\n';
+    return code;
+};
+
 Blockly.JavaScript['move'] = function(block) {
     var value_x = Blockly.JavaScript.valueToCode(block, 'x', Blockly.JavaScript.ORDER_ATOMIC);
     var value_y = Blockly.JavaScript.valueToCode(block, 'y', Blockly.JavaScript.ORDER_ATOMIC);
     var value_z = Blockly.JavaScript.valueToCode(block, 'z', Blockly.JavaScript.ORDER_ATOMIC);
     var value_t = Blockly.JavaScript.valueToCode(block, 't', Blockly.JavaScript.ORDER_ATOMIC);
-    // TODO: Assemble JavaScript into code variable.
-    var code = 'RobotCommands.enablePump(true);\n';
-//    var code = 'RobotCommands.move(' + value_x + ', ' + value_y + ', ' + value_z + ', ' + value_t + ');\n';
-    console.log(code)
-    return code;
-};
-
-Blockly.JavaScript['move_inline'] = function(block) {
-    var value_x = Blockly.JavaScript.valueToCode(block, 'x', Blockly.JavaScript.ORDER_ATOMIC);
-    var value_y = Blockly.JavaScript.valueToCode(block, 'y', Blockly.JavaScript.ORDER_ATOMIC);
-    var value_z = Blockly.JavaScript.valueToCode(block, 'z', Blockly.JavaScript.ORDER_ATOMIC);
-    var value_t = Blockly.JavaScript.valueToCode(block, 't', Blockly.JavaScript.ORDER_ATOMIC);
-    // TODO: Assemble JavaScript into code variable.
-//    var code = 'RobotCommands.move(' + value_x + ', ' + value_y + ', ' + value_z + ', ' + value_t + ');\n';
-    var code = 'RobotCommands.enablePump(true);\n';
-    console.log(code)
+    var code = 'RobotCommands.move(' + value_x + ', ' + value_y + ', ' + value_z + ', ' + value_t + ');\n';
     return code;
 };
 
 Blockly.JavaScript['is_place_free_inline'] = function(block) {
     var value_x = Blockly.JavaScript.valueToCode(block, 'x', Blockly.JavaScript.ORDER_ATOMIC);
     var value_z = Blockly.JavaScript.valueToCode(block, 'z', Blockly.JavaScript.ORDER_ATOMIC);
-    //    var code = 'RobotCommands.isPlaceFree(' + value_x + ', ' + value_z + ');\n';
+//    var code = 'RobotCommands.isPlaceFree(' + value_x + ', ' + value_z + ');\n';
     var code = 'RobotCommands.enablePump(true);\n';
-    console.log(code)
     return code
 };
 
@@ -51,7 +56,6 @@ Blockly.JavaScript['is_place_free'] = function(block) {
     var value_z = Blockly.JavaScript.valueToCode(block, 'z', Blockly.JavaScript.ORDER_ATOMIC);
 //    var code = 'RobotCommands.isPlaceFree(' + value_x + ', ' + value_z + ');\n';
     var code = 'RobotCommands.enablePump(true);\n';
-    console.log(code)
     return code
 };
 
